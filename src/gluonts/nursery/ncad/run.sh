ab9d48978396d5ac0a8c1b368d5867f06b20912d
gplatz <gplatz@phoenixcontact.com>
2022-08-17 17:07:27 +0200

    added slurm

--- src/gluonts/nursery/ncad/run.sh
+++ src/gluonts/nursery/ncad/run.sh
@@ -1,4 +1,18 @@
-python3 examples/article/run_all_experiments.py \
+#!/bin/bash
+#SBATCH --job-name=NCAD
+#SBATCH --time=4-00:00:00 # hh:mm:ss
+#SBATCH --gres="gpu:1"
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --partition=gpu
+#SBATCH --comment="Train some model on SMD"
+#SBATCH --exclude=gpu06,gpu05
+
+
+source activate ./envs
+
+srun python3 examples/article/run_all_experiments.py \
 --ncad_dir='.' \
 --data_dir='./ncad_datasets' \
 --hparams_dir='./examples/article/hparams' \
